4e870a44b56bd98d8b0f22dbc8c695cae8ea1831
unknown <enunessena@gmail.com>
2021-04-18 19:28:03 -0300

    estilizacao da pagina inicial

--- parafuzoapp/src/components/abas/abas.js
+++ parafuzoapp/src/components/abas/abas.js
@@ -3,12 +3,18 @@ import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import TextField from '@material-ui/core/TextField';
-import { DivForm, InputPlaca } from './style';
+import { DivForm, InputPlaca, DivSaida } from './style';
 import Button from '@material-ui/core/Button';
 
 export default function CenteredTabs() {
 
   const [value, setValue] = React.useState(0);
+
+  const tamanhoPaper = () => {
+      if(value === 0){
+        return '279px'
+    }else return '350px'
+  }
 
   const corEntrada = () => {
       if(value === 0){
@@ -37,6 +43,7 @@ export default function CenteredTabs() {
         style={{
           width: '312px',
           height: '67px',
+          margin: '5px',
           backgroundColor: '#28DD91',
           color: 'white'
       }}
@@ -45,7 +52,35 @@ export default function CenteredTabs() {
       </Button>
       </DivForm>
       )
-    }
+    }else return (
+      <DivSaida>
+        <InputPlaca
+          placeholder="AAA-0000"
+        />
+        <Button 
+        style={{
+          width: '312px',
+          height: '67px',
+          margin: '5px',
+          backgroundColor: '#A769B2',
+          color: 'white'
+      }}
+      variant="contained" 
+      >Pagamento
+      </Button>
+      <Button 
+        style={{
+          width: '312px',
+          height: '67px',
+          margin: '5px',
+          backgroundColor: 'white',
+          color: '#A769B2'
+      }}
+      variant="outlined" 
+      >Saída
+      </Button>
+      </DivSaida>
+    )
   }
 
   return (
@@ -53,7 +88,7 @@ export default function CenteredTabs() {
       style={{
         marginTop: '40px',
         width: '344px',
-        height: '279px'
+        height: tamanhoPaper()
       }}
     >
       <Tabs
--- parafuzoapp/src/globalState/globalState.js
+++ parafuzoapp/src/globalState/globalState.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import GlobalStateContext from './globalStateContext'
+import axios from 'axios'
 
 const GlobalState = (props) => {
 
-    const data = {}
+    const entradaEstacionamento = () => {
+        const body = {
+            'plate': 'AAA-2222'
+        }
+
+        axios.post('https://parking-lot-to-pfz.herokuapp.com/parking', body)
+        .then((res) => {
+            console.log(res)
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
+
+    const requests = {entradaEstacionamento}
+
+    const data = {requests}
 
     return(
         <GlobalStateContext.Provider value={data}>
